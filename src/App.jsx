640ad96d353a77e6becded4d64e56773b3cc2b19
Anuradha <anuanya1234@gmail.com>
2026-03-12 13:58:54 -0400

    add ui navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/data/ui/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div className=''>h</div>
+      <div>
+         <Navbar />
+      </div>
     </>
   )
 }
